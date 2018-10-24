a187e46bee02610b47b1366f6226d03078e1f564
Harvey Fletcher <harvey.fletcher1@ntlworld.com>
2018-10-24 09:16:17 +0000

    Because the onelogin service is providing us with a token, we dont need a password column on the users table

--- db_changes/initial/setup.sql
+++ db_changes/initial/setup.sql
@@ -5,7 +5,6 @@ USE reassured_app;
 CREATE TABLE users(
   id INT(10) AUTO_INCREMENT,
   username  VARCHAR(75)  NOT NULL,
-  password  VARCHAR(75)  NOT NULL,
   token     VARCHAR(300) NOT NULL,
   firstname VARCHAR(75)  NOT NULL,
   surname   VARCHAR(75)  NOT NULL,
